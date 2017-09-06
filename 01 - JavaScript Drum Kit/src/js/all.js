233b95312f106b2a70046f798f38bb22a51319ad
Kimi Chang <kchang@trismax.com>
2017-09-06 10:23:30 +0800

    add transition style

--- 01 - JavaScript Drum Kit/src/js/all.js
+++ 01 - JavaScript Drum Kit/src/js/all.js
@@ -1,9 +1,8 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  
   if (!audio) return;  //如果不是audio的data-key則return
-  // console.log(!audio);
-
   key.classList.add('playing');  //如果是audio的data-key，則在class加上"playing"
   audio.currentTime = 0;  //設定目前時間為0
   audio.play();  //播放聲音
@@ -15,6 +14,5 @@ function removeTransition(e) {
 }
 
 const keys = Array.from(document.querySelectorAll('.key'));
-console.log(keys);
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
