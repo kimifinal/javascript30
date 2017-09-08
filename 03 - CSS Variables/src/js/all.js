d6f1fc4d5c62ca7038f51e479d7a994acdab8f41
Kimi Chang <kchang@trismax.com>
2017-09-08 11:02:46 +0800

    Finished 03 - CSS Variables

--- 03 - CSS Variables/src/js/all.js
+++ 03 - CSS Variables/src/js/all.js
@@ -1,18 +1,18 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-  const spacing = document.querySelector('#spacing').value
-  const blur = document.querySelector('#blur').value
-  const base = document.querySelector('#base').value
+  const suffix = this.dataset.sizing || '';  //data-sizing="px"
 
-  console.log('spacing:' + spacing);
-  console.log('blur:' + blur);
-  console.log('base:' + base);
-
-  document.documentElement.style.setProperty(`--${this.name}`, spacing);
+  //style.setProperty() - 將CSS變數與值寫回:root裡
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  // console.log(this.name);
+  // console.log(this.value);
 }
 
+//input有change事件時，執行handleUpdate()
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
+
+//input有mousemove事件時，執行handleUpdate()，此事件可有可無
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 
