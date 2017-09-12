0f4c15ffa1e1c9050b40c47bc75b43ed821d4c6b
Kimi Chang <kchang@trismax.com>
2017-09-12 18:04:50 +0800

    04 - Array Cardio Day 1

--- 04 - Array Cardio Day 1/src/js/all.js
+++ 04 - Array Cardio Day 1/src/js/all.js
@@ -26,16 +26,17 @@
     console.table(fifteen);
     const answer1 = JSON.stringify(fifteen);
     document.querySelector('.A1').innerHTML = 'A1. ' + answer1;
-    let nodeTbody = document.createElement("TBODY");
+    const A1 = document.querySelector('.A1');
+    const A1nodeTable = document.createElement("TABLE");
     for(let i=0; i<fifteen.length; i++){
-        let nodeTr = nodeTbody.appendChild(document.createElement("TR"));
-        for (var j = 0; j < fifteen[i].length; j++) { 
-        let nodeTd = nodeTr.appendChild(document.createElement("TD"));
-        nodeTd.innerHTML = fifteen[i][j];
-        console.log(nodeTd.innerHTML);
-      }
+      const A1nodeTr = document.createElement('tr');
+      A1nodeTr.innerHTML = '<td>' + fifteen[i].first + '</td>' +
+                         '<td>' + fifteen[i].last + '</td>' +
+                         '<td>' + fifteen[i].year + '</td>' +
+                         '<td>' + fifteen[i].passed + '</td>';
+      A1nodeTable.appendChild(A1nodeTr);
     }
-    document.querySelector('.A1').appendChild(nodeTbody);
+    A1.appendChild(A1nodeTable);
 
     // 2. 將inventors內的first與last組合成一個陣列
     // 使用Array.prototype.map()
@@ -50,3 +51,141 @@
 
     // 3. 依據生日由大至小排序所有的inventor
     // 使用Array.prototype.sort()
+
+    // const ordered = inventors.sort(function(a, b) {
+    //   if(a.year > b.year) {
+    //     return 1;
+    //   } else {
+    //     return -1;
+    //   }
+    // });
+    //利用箭頭函式及三元運算式可簡寫如下
+    const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+    console.table(ordered);
+    const answer3 = JSON.stringify(ordered);
+    document.querySelector('.A3').innerHTML = 'A3. ' + answer3;
+    const A3 = document.querySelector('.A3');
+    const A3nodeTable = document.createElement("TABLE");
+    for(let i=0; i<ordered.length; i++){
+      const A3nodeTr = document.createElement('tr');
+      A3nodeTr.innerHTML = '<td>' + ordered[i].first + '</td>' +
+                         '<td>' + ordered[i].last + '</td>' +
+                         '<td>' + ordered[i].year + '</td>' +
+                         '<td>' + ordered[i].passed + '</td>';
+      A3nodeTable.appendChild(A3nodeTr);
+    }
+    A3.appendChild(A3nodeTable);
+
+    // 4. 加總所有inventor的在世時間
+    // 使用Array.prototype.reduce()
+
+    // 傳統寫法
+    // let totalYears = 0;
+    // for (let i = 0; i < inventors.length; i++) {
+    //     let liveYear = inventors[i].passed - inventors[i].year;
+    //     totalYears += liveYear;
+    // }
+
+    const totalYears = inventors.reduce((total, inventor) => {
+      return total + (inventor.passed - inventor.year);
+    }, 0);
+    console.log(totalYears);
+    const A4 = document.querySelector('.A4');
+    A4.innerHTML = 'A4. ' + totalYears;
+
+    // 5. 依據年齡由大至小排序所有的inventor
+    // 使用Array.prototype.sort()
+
+    // const oldest = inventors.sort(function(a, b) {
+    //   const lastInventor = a.passed - a.year;
+    //   const nextInventor = b.passed - b.year;
+    //   if(lastInventor > nextInventor) {
+    //     return 1;
+    //   } else {
+    //     return -1;
+    //   }
+    // });
+
+    const oldest = inventors.sort(function(a, b) {
+      const lastInventor = a.passed - a.year;
+      const nextInventor = b.passed - b.year;
+      return lastInventor > nextInventor ? -1 : 1;
+    });
+    console.table(oldest);
+    const answer5 = JSON.stringify(oldest);
+    document.querySelector('.A5').innerHTML = 'A5. ' + answer5;
+    const A5 = document.querySelector('.A5');
+    const A5nodeTable = document.createElement("TABLE");
+    for(let i=0; i<oldest.length; i++){
+      const yearOld = oldest[i].passed - oldest[i].year;
+      const A5nodeTr = document.createElement('tr');
+      A5nodeTr.innerHTML = '<td>' + oldest[i].first + '</td>' +
+                         '<td>' + oldest[i].last + '</td>' +
+                         '<td>' + ordered[i].year + '</td>' +
+                         '<td>' + ordered[i].passed + '</td>' +
+                         '<td>' + yearOld + '</td>';
+      A5nodeTable.appendChild(A5nodeTr);
+    }
+    A5.appendChild(A5nodeTable);
+
+    // 6. 列出wiki中巴黎所有包含'de'的路名(在wiki中透過querySelectorAll來選取資料作篩選)
+    // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+    // 使用Array.prototype.map() & Array.prototype.filter() + Array.prototype.includes()
+
+    // const category = document.querySelector('.mw-category');
+    // const links = Array.from(category.querySelectorAll('a'));
+    // const de = links
+    //             .map(link => link.textContent)
+    //             .filter(streetName => streetName.includes('de'));
+
+    // 7. 依據lastName排序所有people的資料
+    // 使用Array.prototype.sort() & Array.prototype.split()
+
+    const alpha = people.sort((lastOne, nextOne) => {
+      const [aLast, aFirst] = lastOne.split(', ');
+      const [bLast, bFirst] = nextOne.split(', ');
+      // console.log(aLast + bLast);
+      return aLast > bLast ? 1 : -1;
+    });
+    console.log(alpha);
+    // const answer7 = JSON.stringify(alpha);
+    document.querySelector('.A7').innerHTML = 'A7. ' + alpha;
+    const A7 = document.querySelector('.A7');
+    const A7nodeTable = document.createElement("TABLE");
+    for(let i=0; i<alpha.length; i++){
+      const A7nodeTr = document.createElement('tr');
+      A7nodeTr.innerHTML = '<td>' + alpha[i] + '</td>';
+      A7nodeTable.appendChild(A7nodeTr);
+    }
+    A7.appendChild(A7nodeTable);
+
+    // 8. 分別計算data內每個種類的數量
+    // 使用Array.prototype.reduce()
+
+    const transportation = data.reduce(function(obj, item) {
+      if (!obj[item]) {
+        obj[item] = 0;
+      }
+      // console.log(item);
+      // console.log(obj);
+      obj[item]++;
+      return obj;
+    }, {});
+    console.table(transportation);
+    console.log(transportation.car);
+    const answer8 = JSON.stringify(transportation);
+    document.querySelector('.A8').innerHTML = 'A8. ' + answer8;
+    const A8 = document.querySelector('.A8');
+    const A8nodeTable = document.createElement("TABLE");
+    const A8nodeTr = document.createElement('tr');
+    A8nodeTr.innerHTML = '<div>Car : ' + transportation.car + '</div>' + 
+                         '<div>Truck : ' + transportation.truck + '</div>' +
+                         '<div>Bike : ' + transportation.bike + '</div>' +
+                         '<div>Walk : ' + transportation.walk + '</div>' +
+                         '<div>Van : ' + transportation.van + '</div>' +
+                         '<div>Pogostick : ' + transportation.pogostick + '</div>';
+    A8nodeTable.appendChild(A8nodeTr);
+    A8.appendChild(A8nodeTable);
+    
+    
+    
